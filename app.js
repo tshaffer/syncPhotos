383ea2b34bcbca2dde34a67c4eafa26e1b7f123a
Ted Shaffer <shaffer.family@gmail.com>
2016-12-24 14:55:14 -0800

    added more initial files

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
 console.log("syncPhotos - start");
+console.log("__dirname: ", __dirname);
+
+var pizzaFolder = path.basename('C:\\Users\\Ted\Documents\\PizzaFolder');
+console.log(pizzaFolder);
+
+var pizzaFolder2 = path.win32.basename('C:\\Users\\Ted\Documents\\PizzaFolder');
+console.log(pizzaFolder2);
+
+var testFolder = 'C:\\Users\\Ted\\Documents\\PizzaFolder';
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+})
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  entry: './src/autorun.js',
+  target: 'node',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx$|\.js$/,
+        loader: 'eslint-loader',
+        include: __dirname + '/src',
+        exclude: /build\.js$/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      }
+    ]
+  },
+  // node: {
+  //   console: true,
+  //   fs: 'empty',
+  //   net: 'empty',
+  //   tls: 'empty'
+  // }
+}
